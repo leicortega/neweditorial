e9a7202fe7ca698eecd64386b7c0177cd72dbbe9
FAST <fc089609@gmail.com>
2019-12-07 16:23:20 -0500

    creacion popup of wishlist

---
+++ assets/html/modal_whislist/js.js
@@ -0,0 +1,13 @@
+
+function ejecutar(){
+  //  var html11=document.getElementById("button_open_pop");
+  //  html11.style.display="none";
+    var html=document.getElementById("all_container_pop");
+    html.style.display="block";
+}
+function cerar(){
+   // var html11=document.getElementById("button_open_pop");
+  //  html11.style.display="block";
+    var html=document.getElementById("all_container_pop");
+    html.style.display="none";
+}
