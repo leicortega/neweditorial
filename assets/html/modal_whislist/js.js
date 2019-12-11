2b7d4f93f96d0b9efe7b800efed6f52429c32235
FAST <fc089609@gmail.com>
2019-12-11 00:25:17 -0500

    responsive menu area

--- assets/html/modal_whislist/js.js
+++ assets/html/modal_whislist/js.js
@@ -4,7 +4,18 @@ function fade_in(){
 function fade_Out(){
   $('#all_container_pop').fadeOut(800);
 }
+var vare;
+vare=1;
+function mostrar_menu(){
+  $('#new_responsive_header').fadeIn(800);
+  $('#span2').fadeIn(800);
+  $('#span1').fadeOut(800);
+}
 
-  
+function ocultar_menu(){
+  $('#new_responsive_header').fadeOut(800);
+  $('#span2').fadeOut(800);
+  $('#span1').fadeIn(800);
+}
 
 
