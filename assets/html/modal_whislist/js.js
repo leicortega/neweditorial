1b60a6c20d8abaff5a69061b2517ed5088c1eb04
FAST <fc089609@gmail.com>
2019-12-12 11:32:05 -0500

    modificacion del abaut-us

--- assets/html/modal_whislist/js.js
+++ assets/html/modal_whislist/js.js
@@ -17,4 +17,123 @@ function ocultar_menu(){
   $('#span2').fadeOut(80);
   $('#span1').fadeIn(80);
 }
+var recibe;
+function menu_about(recibe){
+      switch(recibe){
+        case 1:
+          var html1=document.getElementById('container_presentacion');
+          html1.style.display='block';
+          var html2=document.getElementById("container_politica_editorial");
+          html2.style.display="none";
+          var html3=document.getElementById('container_proceso_editorial');
+          html3.style.display='none';
+          var html4=document.getElementById('container_historia');
+          html4.style.display='none';
+          var html5=document.getElementById('container_equipo');
+          html5.style.display='none';
 
+          var active1=document.getElementById('active1');
+          active1.style.background='#d8cea3';
+          var active2=document.getElementById('active2');
+          active2.style.background='#ffffff';
+          var active3=document.getElementById('active3');
+          active3.style.background='#ffffff';
+          var active4=document.getElementById('active4');
+          active4.style.background='#ffffff';
+          var activ5=document.getElementById('active5');
+          activ5.style.background='#ffffff';
+          break;
+        case 2:
+            var html1=document.getElementById('container_presentacion');
+            html1.style.display='none';
+            var html2=document.getElementById("container_politica_editorial");
+            html2.style.display="none";
+            var html3=document.getElementById('container_proceso_editorial');
+            html3.style.display='none';
+            var html4=document.getElementById('container_historia');
+            html4.style.display='block';
+            var html5=document.getElementById('container_equipo');
+            html5.style.display='none';
+
+            var active1=document.getElementById('active1');
+            active1.style.background='#ffffff';
+            var active2=document.getElementById('active2');
+            active2.style.background='#d8cea3';
+            var active3=document.getElementById('active3');
+            active3.style.background='#ffffff';
+            var active4=document.getElementById('active4');
+            active4.style.background='#ffffff';
+            var activ5=document.getElementById('active5');
+            activ5.style.background='#ffffff';
+          break;
+        case 3:
+            var html1=document.getElementById('container_presentacion');
+            html1.style.display='none';
+            var html2=document.getElementById("container_politica_editorial");
+            html2.style.display="block";
+            var html3=document.getElementById('container_proceso_editorial');
+            html3.style.display='none';
+            var html4=document.getElementById('container_historia');
+            html4.style.display='none';
+            var html5=document.getElementById('container_equipo');
+            html5.style.display='none';
+
+            var active1=document.getElementById('active1');
+            active1.style.background='#ffffff';
+            var active2=document.getElementById('active2');
+            active2.style.background='#ffffff';
+            var active3=document.getElementById('active3');
+            active3.style.background='#d8cea3';
+            var active4=document.getElementById('active4');
+            active4.style.background='#ffffff';
+            var activ5=document.getElementById('active5');
+            activ5.style.background='#ffffff';
+          break;
+        case 4:
+            var html1=document.getElementById('container_presentacion');
+            html1.style.display='none';
+            var html2=document.getElementById("container_politica_editorial");
+            html2.style.display="none";
+            var html3=document.getElementById('container_proceso_editorial');
+            html3.style.display='block';
+            var html4=document.getElementById('container_historia');
+            html4.style.display='none';
+            var html5=document.getElementById('container_equipo');
+            html5.style.display='none';
+
+            var active1=document.getElementById('active1');
+            active1.style.background='#ffffff';
+            var active2=document.getElementById('active2');
+            active2.style.background='#ffffff';
+            var active3=document.getElementById('active3');
+            active3.style.background='#ffffff';
+            var active4=document.getElementById('active4');
+            active4.style.background='#d8cea3';
+            var activ5=document.getElementById('active5');
+            activ5.style.background='#ffffff';
+          break;
+        case 5:
+            var html1=document.getElementById('container_presentacion');
+            html1.style.display='none';
+            var html2=document.getElementById("container_politica_editorial");
+            html2.style.display="none";
+            var html3=document.getElementById('container_proceso_editorial');
+            html3.style.display='none';
+            var html4=document.getElementById('container_historia');
+            html4.style.display='none';
+            var html5=document.getElementById('container_equipo');
+            html5.style.display='block';
+
+            var active1=document.getElementById('active1');
+            active1.style.background='#ffffff';
+            var active2=document.getElementById('active2');
+            active2.style.background='#ffffff';
+            var active3=document.getElementById('active3');
+            active3.style.background='#ffffff';
+            var active4=document.getElementById('active4');
+            active4.style.background='#ffffff';
+            var activ5=document.getElementById('active5');
+            activ5.style.background='#d8cea3';
+          break;
+      }
+}
